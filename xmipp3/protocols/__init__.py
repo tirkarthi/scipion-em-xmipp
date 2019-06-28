782c8a6ef6f9003ae1c91fc38079849d95a23f8a
Vilax <jlvilasprieto@gmail.com>
2019-06-28 10:45:58 +0200

    splitting in frames

--- xmipp3/protocols/__init__.py
+++ xmipp3/protocols/__init__.py
@@ -67,6 +67,7 @@ from .protocol_movie_gain import XmippProtMovieGain
 from .protocol_mltomo import XmippProtMLTomo
 from .protocol_movie_average import XmippProtMovieAverage
 from .protocol_movie_correlation import XmippProtMovieCorr
+from .protocol_movie_split_frames import XmippProtSplitFrames
 from .protocol_movie_opticalflow import XmippProtOFAlignment, ProtMovieAlignment
 from .protocol_movie_max_shift import XmippProtMovieMaxShift
 from .protocol_multiple_fscs import XmippProtMultipleFSCs
